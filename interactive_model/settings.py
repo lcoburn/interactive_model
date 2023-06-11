59e1fd5cd2bc22b90bfbdc39cadb853f747af6f0
lcoburn <lukecoburn@gmail.com>
2023-06-11 23:41:38 +0200

    fd

--- interactive_model/settings.py
+++ interactive_model/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'interactive_model/static'),
