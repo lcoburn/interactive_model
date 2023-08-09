58eff6ab2d18a34a531990d3cb2f59386ec4145f
lcoburn <lukecoburn@gmail.com>
2023-08-09 03:32:57 +0200

    trying to get dictionary into geometry loader

--- interactive_model/static/js/model_neighs.js
+++ interactive_model/static/js/model_neighs.js
@@ -1,7 +1,8 @@
 console.log("*** House Neighs Rear ***");
-console.log(Luke);
-console.log(houseInfoStr);
-console.log(houseData.houseInfo);
+// console.log(Luke);
+// console.log(houseInfoStr);
+// console.log(houseData.houseInfo);
+console.log(typeof housesGeometry);
 
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0";
 import { gsap } from "https://cdn.skypack.dev/gsap@3.9.1";
@@ -23,6 +24,7 @@ const scene = new THREE.Scene();
 // var houseJsonPath = scriptTag.getAttribute('house.json');
 const loader = new JsonGeometryLoader(
     "https://lukecoburn.s3.eu-west-2.amazonaws.com/house_neighs.json"
+    // housesGeometry
 );
 
 loader.load().then((geometries) => {
@@ -86,114 +88,114 @@ loader.load().then((geometries) => {
     // tl.fromTo(cube.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 })
 });
 
-var cube;
-var edges;
-var line;
-var width_box;
-var depth_box;
-var height_box;
-var box3;
-var size;
-var positions;
-var initialPosition;
-var ext_type = "rear";
+// var cube;
+// var edges;
+// var line;
+// var width_box;
+// var depth_box;
+// var height_box;
+// var box3;
+// var size;
+// var positions;
+// var initialPosition;
+// var ext_type = "rear";
 
-// var extensions = document.body.getAttribute('data-background');
-const loader1 = new JsonGeometryLoader(
-    "https://lukecoburn.s3.eu-west-2.amazonaws.com/extensions3.json"
-);
+// // var extensions = document.body.getAttribute('data-background');
+// const loader1 = new JsonGeometryLoader(
+//     "https://lukecoburn.s3.eu-west-2.amazonaws.com/extensions3.json"
+// );
 
-loader1.load().then((geometries) => {
-    geometries.forEach((geometry, index) => {
-        // Create a material
-        const material = new THREE.MeshStandardMaterial({
-            color: 0x00ff00,
-            transparent: true,
-            opacity: 1,
-        });
-        // Create a mesh with the geometry and material
-        cube = new THREE.Mesh(geometry, material);
-        // edges = new THREE.EdgesGeometry(cube.geometry);
-        // line = new THREE.LineSegments(
-        //     edges,
-        //     new THREE.LineBasicMaterial({ color: 0xffffff })
-        // );
-        // cube.add(line);
-        // Add the mesh to the scene
-        scene.add(cube);
-    });
+// loader1.load().then((geometries) => {
+//     geometries.forEach((geometry, index) => {
+//         // Create a material
+//         const material = new THREE.MeshStandardMaterial({
+//             color: 0x00ff00,
+//             transparent: true,
+//             opacity: 1,
+//         });
+//         // Create a mesh with the geometry and material
+//         cube = new THREE.Mesh(geometry, material);
+//         // edges = new THREE.EdgesGeometry(cube.geometry);
+//         // line = new THREE.LineSegments(
+//         //     edges,
+//         //     new THREE.LineBasicMaterial({ color: 0xffffff })
+//         // );
+//         // cube.add(line);
+//         // Add the mesh to the scene
+//         scene.add(cube);
+//     });
 
-    // Create a new GUI
-    var gui = new GUI({ autoPlace: false });
-    gui.domElement.id = "gui";
-    var customContainer = document.getElementById("my-gui-container-1");
-    customContainer.appendChild(gui.domElement);
+//     // Create a new GUI
+//     var gui = new GUI({ autoPlace: false });
+//     gui.domElement.id = "gui";
+//     var customContainer = document.getElementById("my-gui-container-1");
+//     customContainer.appendChild(gui.domElement);
 
-    // Append it to your container
-    document.getElementById("my-gui-container-1").appendChild(gui.domElement);
+//     // Append it to your container
+//     document.getElementById("my-gui-container-1").appendChild(gui.domElement);
 
-    box3 = new THREE.Box3().setFromObject(cube);
-    size = new THREE.Vector3();
-    box3.getSize(size);
+//     box3 = new THREE.Box3().setFromObject(cube);
+//     size = new THREE.Vector3();
+//     box3.getSize(size);
 
-    // Dimensions
-    width_box = size.x;
-    height_box = size.y;
-    depth_box = size.z;
+//     // Dimensions
+//     width_box = size.x;
+//     height_box = size.y;
+//     depth_box = size.z;
 
-    // Vertices
-    positions = cube.geometry.attributes.position;
-    // Print out the vertices
-    for (let i = 0; i < positions.count; i++) {
-        let x = positions.getX(i);
-        let y = positions.getY(i);
-        let z = positions.getZ(i);
-        // console.log(`Vertex ${i}: X = ${x}, Y = ${y}, Z = ${z}`);
-    }
+//     // Vertices
+//     positions = cube.geometry.attributes.position;
+//     // Print out the vertices
+//     for (let i = 0; i < positions.count; i++) {
+//         let x = positions.getX(i);
+//         let y = positions.getY(i);
+//         let z = positions.getZ(i);
+//         // console.log(`Vertex ${i}: X = ${x}, Y = ${y}, Z = ${z}`);
+//     }
 
-    // Create an object for the cube's dimensions
-    var cubeDimensions = {
-        width: width_box,
-        depth: depth_box,
-        height: height_box,
-        color1: "#00ff00",
-        color2: "#ffa500",
-        color3: "#ff0000",
-    };
+//     // Create an object for the cube's dimensions
+//     var cubeDimensions = {
+//         width: width_box,
+//         depth: depth_box,
+//         height: height_box,
+//         color1: "#00ff00",
+//         color2: "#ffa500",
+//         color3: "#ff0000",
+//     };
 
-    // Get initial position
-    initialPosition = new THREE.Vector3(
-        positions.getX(0),
-        positions.getY(0),
-        positions.getZ(0)
-    );
+//     // Get initial position
+//     initialPosition = new THREE.Vector3(
+//         positions.getX(0),
+//         positions.getY(0),
+//         positions.getZ(0)
+//     );
 
-    CubeUpdater.updateCubeDepth(
-        cube,
-        cubeDimensions,
-        initialPosition,
-        ext_type
-    );
+//     CubeUpdater.updateCubeDepth(
+//         cube,
+//         cubeDimensions,
+//         initialPosition,
+//         ext_type
+//     );
 
-    // Add sliders to the GUI
-    gui.add(cubeDimensions, "depth", 2, 7).onChange((depth) =>
-        CubeUpdater.updateCubeDepth(
-            cube,
-            { ...cubeDimensions, depth },
-            initialPosition,
-            ext_type
-        )
-    );
+//     // Add sliders to the GUI
+//     gui.add(cubeDimensions, "depth", 2, 7).onChange((depth) =>
+//         CubeUpdater.updateCubeDepth(
+//             cube,
+//             { ...cubeDimensions, depth },
+//             initialPosition,
+//             ext_type
+//         )
+//     );
 
-    gui.add(cubeDimensions, "width", 3, 6.5).onChange((width) =>
-        CubeUpdater.updateCubeWidth(
-            cube,
-            { ...cubeDimensions, width },
-            initialPosition,
-            ext_type
-        )
-    );
-});
+//     gui.add(cubeDimensions, "width", 3, 6.5).onChange((width) =>
+//         CubeUpdater.updateCubeWidth(
+//             cube,
+//             { ...cubeDimensions, width },
+//             initialPosition,
+//             ext_type
+//         )
+//     );
+// });
 
 // Sizes
 const width = document.getElementById("model-container-2").clientWidth;
@@ -221,14 +223,14 @@ scene.add(camera);
 
 // Renderer
 const canvas = document.querySelector(".webgl-2");
-console.log(canvas);
+// console.log(canvas);
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(2);
 renderer.render(scene, camera);
 
 function clickButton(jsonFileLocation) {
-    console.log("yay");
+    // console.log("yay");
     const loader = new JsonGeometryLoader(jsonFileLocation);
     loader.load().then((geometries) => {
         // see below
